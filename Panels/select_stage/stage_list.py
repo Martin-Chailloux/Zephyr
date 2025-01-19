917ebefdabb643c094a4bb523c422ab54dd2a0cc
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-01-19 14:00:28 +0100

    feat: stagelist v002

--- Panels/select_stage/stage_list.py
+++ Panels/select_stage/stage_list.py
@@ -1,25 +1,31 @@
+from textwrap import dedent
+
 import qtawesome
 
 from PySide6 import QtCore
-from PySide6.QtCore import Signal
-from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QHBoxLayout,
-                               QSizePolicy, QComboBox)
+from PySide6.QtCore import Signal, QSize
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QSizePolicy, QComboBox)
 
 from MangoEngine.document_models import Status
 from Widgets.line_edit_popup import LineEditPopup
-from Widgets.qwidgets_extensions import ZIconButton, ZTransparentIconButton
+from Widgets.qwidgets_extensions import ZTransparentIconButton, ZIconButton
 from Widgets.status_widgets import ZStatusSelector
 
 
 class StageItem(QWidget):
+    # TODO: paint an icon and a color at the left edge of the item
     h = 26
     moved = Signal()
+    stage_selected = Signal(str)
+
     def __init__(self, name: str, status: str):
         self.name = name
         self.status = status
         super().__init__()
 
         self._init_ui()
+        self.connect_signals()
 
     def __repr__(self):
         return f"StageItem|{self.name}"
@@ -32,40 +38,59 @@ class StageItem(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(2)
 
-        # mover = ZIconButton(icon_name="mdi.arrow-up-bold", width=self.h, icon_size=18)
-        mover = ZTransparentIconButton(icon_name="mdi.arrow-up-bold", width=self.h, icon_size=18)
+        mover = ZTransparentIconButton(icon_name="mdi.arrow-up-bold", width=18, icon_size=18)
         mover.clicked.connect(self.moved.emit)
         layout.addWidget(mover)
+
+        # TODO: photo de profil comme icone, le nom en tooltip quand on hover
+        # TODO: bigger round button with tooltip
+        user_combobox = QComboBox()
+        layout.addWidget(user_combobox)
+        user_combobox.setFixedHeight(self.h)
+        users = ["Martin", "Kim", "Elise", "Chloé", "Hugo", "Camille"]
+        user_combobox.addItems(users)
+        for i, user in enumerate(users):
+            icon_path = f"Icons/Users/{user.lower()}.jpg"
+            icon = QIcon(icon_path)
+            user_combobox.setItemIcon(i, icon)
+            user_combobox.setToolTip("UserTest")
 
         button = QPushButton(self.name)
         layout.addWidget(button)
         button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         button.setCheckable(True)
+        button.setStyleSheet(dedent("""
+            QPushButton {color: darkgrey}
+            QPushButton:checked {
+                color: white;
+                border: 1px solid white;
+            }
+        """
+        ))
 
-        # TODO: photo de profil comme icone, le nom en tooltip quand on hover
-        user_combobox = QComboBox()
-        layout.addWidget(user_combobox)
-        users = ["Martin", "Kim", "Elise", "Chloé", "Hugo", "Camille"]
-        user_combobox.addItems(users)
-
-        status = ZStatusSelector(starting_status=self.status)
-        layout.addWidget(status)
+        status_button = ZStatusSelector(height=self.h, starting_status=self.status)
+        layout.addWidget(status_button)
 
         button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        # for widget in [user_combobox, status]:
-        #     # widget.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        #     widget.setFixedHeight(self.h + 12)
 
         # public vars
         self.mover = mover
+        self.button = button
 
+    def connect_signals(self):
+        self.button.clicked.connect(self.on_button_clicked)
+
+    def on_button_clicked(self):
+        if self.button.isChecked():
+            text = self.button.text()
+            self.stage_selected.emit(text)
 
 class ZStageListWidget(QWidget):
     defaults = {
-        "modeling": [],
-        "rigging": [],
-        "texturing": [],
-        "shading": [],
+        "modeling": "ph.cube-fill",
+        "rigging": "mdi.human-edit",
+        "texturing": "fa.paint-brush",
+        "shading": "mdi6.crystal-ball",
     }
 
     create_stage_request = Signal(str)
@@ -76,6 +101,7 @@ class ZStageListWidget(QWidget):
         self.stage_items: list[StageItem] = []
 
         self._init_ui()
+        self.add_stages()
 
     def _init_ui(self):
         layout = QVBoxLayout()
@@ -84,24 +110,45 @@ class ZStageListWidget(QWidget):
         layout.setContentsMargins(self.margin, self.margin, self.margin, self.margin)
         layout.setSpacing(5)
 
-        new_button = QPushButton(" New stage")
-        layout.addWidget(new_button)
+        stage_layout = QVBoxLayout()
+        layout.addLayout(stage_layout)
+        stage_layout.setContentsMargins(3, 3, 3, 3)
+
+        buttons_layout = QHBoxLayout()
+        layout.addLayout(buttons_layout)
+        buttons_layout.setSpacing(2)
+
+        h: int = 26
+        # 'New' button
+        new_button = QPushButton(" Edit")
+        buttons_layout.addWidget(new_button)
         new_button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        new_button.setIcon(qtawesome.icon("fa.plus-circle"))
+        new_button.setIcon(qtawesome.icon("mdi.format-list-checkbox"))
+        new_button.setIconSize(QSize(22, 22))
         new_button.clicked.connect(self.create_popup)
+
+        new_button.setFixedHeight(h)
+
+        # public vars
+        self.stage_layout = stage_layout
+        self.buttons_layout = buttons_layout
         self.new_button = new_button
 
-        for stage_name in self.defaults.keys():
-            self.add_stage_item(stage_name, Status.WFA)
-
-    def add_stage_item(self, name, status=Status.TODO):
+    def add_stage_item(self, name, status: str=Status.TODO, icon_name: str=None):
         stage_item = StageItem(name=name, status=status)
-        self.layout().addWidget(stage_item)
-        self.layout().insertWidget(-1, self.new_button)
+        if icon_name is not None:
+            stage_item.button.setIcon(qtawesome.icon(icon_name))
+
+        self.stage_layout.addWidget(stage_item)
+        self.stage_items.append(stage_item)
 
         stage_item.moved.connect(self.on_moved_requested)
+        stage_item.stage_selected.connect(self.on_stage_selected)
 
-        self.stage_items.append(stage_item)
+    def add_stages(self):
+        self.stage_items = []
+        for stage_name, icon_name in self.defaults.items():
+            self.add_stage_item(name=stage_name, icon_name=icon_name)
 
     def create_popup(self):
         popup = LineEditPopup(title="New Stage",
@@ -113,11 +160,12 @@ class ZStageListWidget(QWidget):
         self.create_stage_request.emit(description)
 
     def on_moved_requested(self):
+        sender: StageItem = self.sender()
+
         before: list[StageItem] = self.stage_items
         after: list[StageItem] = []
 
         for i, stage_item in enumerate(self.stage_items):
-            sender: StageItem = self.sender()
             if stage_item is sender:
                 before = self.stage_items[:i]
                 after = self.stage_items[i+1:]
@@ -129,8 +177,14 @@ class ZStageListWidget(QWidget):
         self.stage_items = before + after
         self.refresh()
 
+    def on_stage_selected(self, stage_name: str):
+        print(f"Selected stage: {stage_name}")
+        sender: StageItem = self.sender()
+        for i, stage_item in enumerate(self.stage_items):
+            if stage_item is not sender:
+                stage_item.button.setChecked(False)
+
     def refresh(self):
         for stage_item in self.stage_items:
-            self.layout().addWidget(stage_item)
-        self.layout().addWidget(self.new_button)
+            self.stage_layout.addWidget(stage_item)
 
