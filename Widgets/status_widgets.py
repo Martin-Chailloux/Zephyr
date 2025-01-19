fe426411c8ee74aa9ed059fbe55438e0dca3f004
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-01-19 14:01:34 +0100

    feat: good looking context menu

--- Widgets/status_widgets.py
+++ Widgets/status_widgets.py
@@ -1,17 +1,18 @@
 from textwrap import dedent
 
-from PySide6 import QtCore
-from PySide6.QtCore import QModelIndex, QPoint, QSize, Signal
-from PySide6.QtGui import QStandardItemModel, QColor, QCursor
-from PySide6.QtWidgets import QComboBox, QListView, QWidget, QVBoxLayout, QPushButton, QDialog, QGridLayout, \
-    QHBoxLayout, QSizePolicy
+from PySide6.QtCore import QPoint, QSize, Signal
+from PySide6.QtGui import QColor, QCursor
+from PySide6.QtWidgets import QPushButton, QGridLayout
 from qtpy.QtWidgets import QMenu
 
 from Gui.palette_api import ZPalette
 
 c = ZPalette()
 
-class ZStatusSelector(QComboBox):
+class ZStatusSelector(QPushButton):
+    # TODO: les choix doivent venir de la db
+    # TODO: disabled style
+
     colors = {
         "WAIT": c.text_white,
         "TODO": c.yellow,
@@ -22,61 +23,41 @@ class ZStatusSelector(QComboBox):
         "OMIT": c.light,
     }
 
-    def __init__(self, starting_status: str = "TODO"):
-        # TODO: les choix doivent venir de la db
+    w: int = 46
+
+    def __init__(self, height: int = 26, starting_status: str = "TODO"):
         super().__init__()
-        self.addItems([k for k in self.colors.keys()])
-        self.view().colors = self.colors
+        self.h = height
+        self.setFixedSize(self.w, self.h)
+        self.set_new_status(text=starting_status)
 
-        self.on_text_changed(self.currentText())
-        self.currentTextChanged.connect(self.on_text_changed)
-
-        for i, color in enumerate(self.colors.values()):
-            self.setItemData(i, color, QtCore.Qt.ItemDataRole.BackgroundRole)
-            self.setItemData(i, QColor("black"), QtCore.Qt.ItemDataRole.ForegroundRole)
-
-        if starting_status in self.colors.keys():
-            self.setCurrentText(starting_status)
-
-        # self.customContextMenuRequested.connect()
-
-    def on_text_changed(self, text):
+    def set_new_status(self, text: str):
+        success = False
         for name, color in self.colors.items():
-            color = color.name(format=QColor.NameFormat.HexArgb)
             if text == name:
-                self.setStyleSheet(dedent("""
-                    QComboBox {
-                        background-color: $color;
-                        color: black;
-                        }
-                """.replace("$color", color)))
-
-        unused = """
-                    QListView::item:selected {
-                        background-color: transparent;
-                        color: white;
-                        }
-        """
+                self.setText(text)
+                set_stylesheet(self, color)
+                success = True
+        if not success:
+            raise ValueError("Unknown status, cannot set.")
 
     def mousePressEvent(self, event):
         self.create_menu()
-        # if event.button() == QtCore.Qt.MouseButton.RightButton:
-        # else:
-        #     super().mousePressEvent(event)
 
     def create_menu(self):
-        menu = SelectStatusMenu()
-        menu.status_selected.connect(self.setCurrentText)
+        menu = SelectStatusMenu(button_w = self.w, button_h = self.h + 8)
+        menu.status_selected.connect(self.set_new_status)
 
         x = int(QCursor.pos().x() - menu.w / 2)
         y = int(QCursor.pos().y() - menu.h / 2)
         menu.exec(QPoint(x, y))
-        
+
 
 class SelectStatusMenu(QMenu):
-    w: int = 120
-    h: int = 120
+    margin = 10
+    spacing = 1
     max_columns: int = 2
+    status_selected = Signal(str)
 
     colors = {
         "TODO": c.yellow,
@@ -88,44 +69,47 @@ class SelectStatusMenu(QMenu):
         "OMIT": c.light,
     }
 
-    status_selected = Signal(str)
-
-    def __init__(self):
+    def __init__(self, button_w: int, button_h: int):
         super().__init__()
+        self.max_rows = int(len(self.colors) / self.max_columns)
+
         layout = QGridLayout()
         self.setLayout(layout)
-        layout.setContentsMargins(10, 10, 10, 10)
-        layout.setSpacing(1)
+        layout.setContentsMargins(self.margin, self.margin, self.margin, self.margin)
+        layout.setSpacing(self.spacing)
 
+        self.w = (button_w * self.spacing * self.max_columns) + (2 * self.margin)
+        self.h = (button_h * self.spacing * self.max_rows) + (2 * self.margin)
         self.setFixedSize(QSize(self.w, self.h))
 
         for i, (text, color) in enumerate(self.colors.items()):
-            print(i, text, color)
             button = QPushButton(text)
-            c_base = color.name()
-            c_hover = color.lighter(110).name()
-            button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-            button.setStyleSheet(dedent("""
-                QPushButton {
-                    color: black ;
-                    background-color:$c_base;
-                    border: 3px solid transparent;
-                }
-                QPushButton:hover {
-                    background-color:$c_hover;
-                    border: none;
-                }
-                """)
-                .replace("$c_base", c_base)
-                .replace("$c_hover", c_hover))
+            set_stylesheet(button, color)
+            button.clicked.connect(self.on_button_clicked)
 
             row = int(i / self.max_columns)
             column = i % self.max_columns
-            button.clicked.connect(self.on_button_clicked)
-
             layout.addWidget(button, row, column)
 
     def on_button_clicked(self):
         button: QPushButton = self.sender()
         self.status_selected.emit(button.text())
         self.close()
+
+
+def set_stylesheet(widget, color: QColor):
+    c_base = color.name()
+    c_hover = color.lighter(110).name()
+    widget.setStyleSheet(dedent("""
+                    QPushButton {
+                        color: black ;
+                        background-color:$c_base;
+                        border: 2px solid transparent;
+                    }
+                    QPushButton:hover {
+                        background-color:$c_hover;
+                        border: none;
+                    }
+                    """)
+                    .replace("$c_base", c_base)
+                    .replace("$c_hover", c_hover))
