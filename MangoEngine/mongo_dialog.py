d4f5d9db37bbd7dd9fe40397e0a02e97ca76aebf
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-02-09 12:45:21 +0100

    feat:  up to date Asset + StageTemplate + Stage

--- MangoEngine/mongo_dialog.py
+++ MangoEngine/mongo_dialog.py
@@ -1,4 +1,4 @@
-from MangoEngine.document_models import Asset, Stage
+from MangoEngine.document_models import Asset, Stage, StageTemplate
 from Utils.chronometer import Chronometer
 
 
@@ -11,31 +11,57 @@ def text_to_input(text: str):
 
 
 def create_asset(category: str, name : str, variant: str = None, **kwargs) -> Asset:
-    kwargs = dict(name=name, category=category, variant=variant, **kwargs)
+    v = variant or "-"
+    longname = "_".join(s for s in [category, name, v])
+    kwargs = dict(name=name, category=category, variant=variant, longname=longname, **kwargs)
     kwargs = {k: v for k, v in kwargs.items() if v is not None}
     asset = Asset(**kwargs)
     asset.save()
-    print(f"Created: {asset}")
+    print(f"Created: {asset.__repr__()}")
     return asset
 
 
-def get_asset(category: str = None, name: str = None, variant: str = None, **kwargs) -> list[Asset]:
+def get_asset(category: str = None, name: str = None, variant: str = None, unique=False, **kwargs) -> list[Asset] | Asset:
     chronometer = Chronometer()
     kwargs = dict(category=category, name=name, variant=variant, **kwargs)
     kwargs = {k: v for k, v in kwargs.items() if v is not None}
-    assets = list(Asset.objects(**kwargs))
 
-    print(f"Found {len(assets)} Asset in {chronometer.tick()}:")
-    for asset in assets:
+    if unique:
+        asset = Asset.objects.get(**kwargs)
+        print(f"Found 1 unique Asset in {chronometer.tick()}:")
         print("    -", asset.__repr__())
-    return assets
+        return asset
+
+    else:
+        assets = list(Asset.objects(**kwargs))
+        print(f"Found {len(assets)} Asset in {chronometer.tick()}:")
+        for asset in assets:
+            print("    -", asset.__repr__())
+        return assets
 
 
-def create_stage(asset: Asset, description: str, detail: str=None, **kwargs) -> Stage:
-    kwargs = dict(asset=asset, description=description, detail=detail, **kwargs)
+# NOTE: currently no gui to create stages
+def create_stage_template(name: str, label: str, description: str,
+                          color: str = None, icon_name: str = None, **kwargs) -> StageTemplate:
+    kwargs = dict(name=name, label=label, description=description,
+                  color=color, icon_name=icon_name, **kwargs)
+    kwargs = {k: v for k, v in kwargs.items() if v is not None}
+    stage_template = StageTemplate(**kwargs)
+    stage_template.save()
+    print(f"Created: {stage_template.__repr__()}")
+    return stage_template
+
+
+def create_stage(stage_template: StageTemplate, asset: Asset, **kwargs) -> Stage:
+    #longname TODO: longname = {asset.longname}_{stage_template.name}
+    longname = "_".join(s for s in [stage_template.name, asset.longname])
+    kwargs = dict(stage_template=stage_template, asset=asset, longname=longname, **kwargs)
     kwargs = {k: v for k, v in kwargs.items() if v is not None}
     stage = Stage(**kwargs)
     stage.save()
-    print(f"Created: {stage}")
+    print(f"Created: {stage.__repr__()}")
+
+    stage.append_to_asset()
+
     return stage
 
