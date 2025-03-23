faf11af4138bac183fd17d150d242f9db9b7b5b6
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-03-23 16:48:25 +0100

    feat [blender_io]: open, open_interactive, save, save_as

--- Softwares/Abstract/abstract_io.py
+++ Softwares/Abstract/abstract_io.py
@@ -1,11 +1,19 @@
 
 
 class AbstractSoftwareFile:
+    exe_path: str = ""
+
     def __init__(self, filepath: str):
         self.filepath = filepath
+
+    def open_interactive(self):
+        pass
 
     def open(self):
         pass
 
     def save(self):
+        pass
+
+    def save_as(self, filepath: str):
         pass
--- Softwares/Blender/blender_io.py
+++ Softwares/Blender/blender_io.py
@@ -1,24 +1,39 @@
+import subprocess
 import bpy
 
 from Softwares.Abstract.abstract_io import AbstractSoftwareFile
 
 
 class BlenderFile(AbstractSoftwareFile):
+    exe_path: str = "C:/Program Files/Blender Foundation/Blender 4.3/blender-launcher.exe"
+    
     def __init__(self, filepath: str):
         super().__init__(filepath=filepath)
+
+    def open_interactive(self):
+        subprocess.Popen([self.exe_path, self.filepath])
 
     def open(self):
         bpy.ops.wm.open_mainfile(filepath=self.filepath)
 
     def save(self):
+        bpy.ops.wm.save_mainfile(filepath=self.filepath)
+
+    def save_as(self, filepath: str):
         # TODO: testing
-        #  maybe this should be save_as()
+        self.filepath = filepath
         bpy.ops.wm.save_as_mainfile(filepath=self.filepath)
 
 
 if __name__ == "__main__":
-    import subprocess
-    # subprocess.Popen(['Blender', filepath])
+    blender_file = BlenderFile(filepath="C:/Users/marti/OneDrive/Documents/__work/_dev/zephyr_externals/palette.blend")
 
-    blender_file = BlenderFile(filepath="C:\\Users\marti\OneDrive\Documents\__work\_dev\zephyr_externals\palette.blend")
     blender_file.open()
+
+    # test: create collection
+    collection = bpy.data.collections.new("MyTestCollection")
+    bpy.context.scene.collection.children.link(collection)
+    # ------------------------
+
+    blender_file.save()
+    blender_file.open_interactive()
