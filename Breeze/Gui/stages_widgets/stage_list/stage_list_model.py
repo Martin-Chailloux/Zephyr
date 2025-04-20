1ea0b54e0d083c9ae10992341a8170946d9e6c4d
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-04-20 22:07:22 +0200

    fix: also refresh stage list when it there is a single item

--- Breeze/Gui/stages_widgets/stage_list/stage_list_model.py
+++ Breeze/Gui/stages_widgets/stage_list/stage_list_model.py
@@ -29,10 +29,11 @@ class StageListModel(AbstractListModel):
 
     def set_asset(self, asset: Asset):
         self.asset = asset
+        self.clear()
+
         if asset is None:
             return
 
-        self.clear()
         for stage in asset.stages:
             self.add_item(stage=stage)
 
@@ -51,9 +52,8 @@ class StageListModel(AbstractListModel):
 
     def refresh(self):
         stages = [item.data(StageItemRoles.stage) for item in self.items]
-        if len(stages) <= 1:
-            return
-        stages = Asset.objects.get(longname=stages[0].asset.longname).stages  # query the data from db else it is not up to date
+        asset = Asset.objects.get(longname=stages[0].asset.longname)
+        stages = asset.stages  # query the data from db else it is not up to date
 
         self.blockSignals(True)
         self.clear()
