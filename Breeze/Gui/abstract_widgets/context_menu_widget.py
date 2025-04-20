bb6f19e390a4b746a3d565d436fe1f008607f8ed
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-04-20 22:02:08 +0200

    feat [ContextMenuWidget]: cancel var and optional fixed size

--- Breeze/Gui/abstract_widgets/context_menu_widget.py
+++ Breeze/Gui/abstract_widgets/context_menu_widget.py
@@ -8,16 +8,20 @@ from PySide6.QtWidgets import QDialog
 
 
 class ContextMenuWidget(QDialog):
-    def __init__(self, w: int, h: int, remove_borders: bool=True,
+    def __init__(self, w: int, h: int, show_borders: bool=False,
                  position: list[float] = None):
         super().__init__()
         self.w = w
         self.h = h
         self.position = position or [0, 0]
+        self.is_canceled: bool=False
 
-        if remove_borders:
+        if not show_borders:
             self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
-        self.setFixedSize(QSize(w, h))
+        if w is not None:
+            self.setFixedWidth(w)
+        if h is not None:
+            self.setFixedHeight(h)
 
     def exec(self):
         x = int(QCursor.pos().x())
@@ -34,4 +38,5 @@ class ContextMenuWidget(QDialog):
         super().mousePressEvent(event)
         if isinstance(event, QMouseEvent):
             if event.button() == QtCore.Qt.MouseButton.RightButton:
+                self.is_canceled = True
                 self.close()
