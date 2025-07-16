86d54b2bb0ca213447158fafc6576d830d0141af
Martin CHAILLOUX <martin.chailloux.pro@gmail.com>
2025-07-16 14:12:23 +0200

    todo

--- Sandbox/todo.py
+++ Sandbox/todo.py
@@ -1,10 +1,32 @@
 
-# TODO: Browser -> selected stage panel
-#  - checkboxes for each panel
-#  - export-components gui for stage
+# TODO: auto-refresh for versions lists / stage exports panel / jobs
+
+# TODO: turbine:
+#  - ui for the process step has the same ui than others: pill + logs
+#  - show jobs failure / success in jobs list
+
+# TODO: users
+#  - per project information
+#    - roles (sup / lead / graphist / pipe)
+#    - favorite_assets
+#    - recent_assets
+#    --> probably a Contributor/Member/PrUser/SubUser document in project_documents, with source_user and the needed extra fields
+#       it is a user with extra project-related fields
+#       can be found with BreezeApp.sub_user
+
+# TODO: asset browser
+#  - favorites / recent
+
+# TODO: breeze app
+#  - gui to change user / project
+#  - qnd create users / projects
 
 # TODO: confo common ui components
 #  - confo popup code for asset creation
+#  - confo + improve stage templates assignment to stages
+
+# TODO: Browser -> selected stage panel
+#  - exports context menu
 
 # TODO: software classes
 #  subclass: in_out for import / export
